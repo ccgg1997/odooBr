7ee90f76bf8b7d46b483ca8d2ade3b7976e268b7
cgomez <cgomez@truora.com>
2025-09-13 13:21:52 -0500

    dockerfile

---
+++ custom-addons/invoice_custom/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Invoice Custom",
+    "version": "1.0",
+    "summary": "Agrupa variantes en líneas de factura y las muestra en una sola línea",
+    "author": "TuNombre",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/report_invoice_document.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
